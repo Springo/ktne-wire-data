2bbd65bb8458357bbf27f5a321a8c4cfb3fe74ce
Kevin Xia <kevinmxia@gmail.com>
2018-01-15 01:29:11 -0500

    Add basic code template.

---
+++ KTNEWires.py
@@ -0,0 +1,24 @@
+"""
+AUTHORS: Kevin Xia
+
+PURPOSE:
+    Creating data based on the wire module from Keep Talking and Nobody Explodes
+
+DEVELOPER NOTES:
+    Author and program are not affiliated with KTNE in any way.
+"""
+
+# =============================================================================
+# Libraries and Global Variables
+# =============================================================================
+
+import csv
+
+# =============================================================================
+
+class KTNEWires:
+    """ KTNE wire data class """
+
+    def __init__(self):
+        """ Initialize fields here """
+        
