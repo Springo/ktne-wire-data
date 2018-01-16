d95fe62197ab3ce60719968b5f957a84cebf7783
Kevin Xia <kevinmxia@gmail.com>
2018-01-15 19:07:24 -0500

    Added write to csv functionality.

--- Tester.py
+++ Tester.py
@@ -30,13 +30,35 @@ def get_data(filename, head=True):
             data.append(line)
     return data, header
 
+def write_results(filename, data, labels=True):
+    with open(filename, 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        header = ['Wire Count',
+                  'Wire 1',
+                  'Wire 2',
+                  'Wire 3',
+                  'Wire 4',
+                  'Wire 5',
+                  'Wire 6',
+                  'Red Wire Count',
+                  'Blue Wire Count',
+                  'Yellow Wire Count',
+                  'White Wire Count',
+                  'Black Wire Count',
+                  'Serial Number'
+                  ]
+        if labels:
+            header.append('Cut Wire')
+        writer.writerow(header)
+        for i in range(len(data)):
+            writer.writerow(data[i])
+
 def main():
     """ Main method for initializing a run """
 
     KW = KTNEWires()
-    data = KW.generate_data(100)
-    for row in data:
-        print(row)
+    data = KW.generate_data(1000)
+    write_results("mock_data_train.csv", data)
 
 if __name__ == "__main__":
     """ Run main if this python file is executed """
